2c28bddf75835d77c852dae44d876355abe11336
huoke <changfeng@51huxin.com>
2020-03-17 19:47:53 +0800

    init

--- app/src/main/java/com/dream/souyizi/MainActivity.java
+++ app/src/main/java/com/dream/souyizi/MainActivity.java
@@ -1,7 +1,8 @@
 package com.dream.souyizi;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
